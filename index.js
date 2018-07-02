ad3bfd4494f355f18a27f4713418c2b17de5861f
marekbrainhub <marek@brainhub.pl>
2018-07-02 15:19:36 +0200

    Adding console messages.

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ const questions = require('./questions');
 const pwd = process.cwd();
 
 inquirer.prompt(questions).then(answers => {
+  console.log('Creating the project directory...');
   const projectPath = path.resolve(pwd, answers.projectName);
   fs.mkdirSync(projectPath);
+
+  console.log('Copying template files into the project directory...');
   createDirectoryContents('./templates', projectPath, answers);
+
+  console.log('Removing residual files...');
   clearEmptyFiles(projectPath);
+
+  console.log('Done!\n');
+  console.log('You can now enter your project and start development:\n');
+  console.log(`   cd ${answers.projectName} && npm install`);
+  console.log('   npm run dev');
+  console.log('\n\n');
 });
